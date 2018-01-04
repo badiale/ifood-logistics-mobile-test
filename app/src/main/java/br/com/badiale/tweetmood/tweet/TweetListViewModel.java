2d7b293fa1806e4c324b920b1f88669ad18d3641
Lucas Badiale <lcbadial@gmail.com>
2018-01-03 23:44:42 -0200

    Adiciona Firebase Analytics, Crashreport e Crashlytics (beta)

--- app/src/main/java/br/com/badiale/tweetmood/tweet/TweetListViewModel.java
+++ app/src/main/java/br/com/badiale/tweetmood/tweet/TweetListViewModel.java
@@ -1,8 +1,12 @@
 package br.com.badiale.tweetmood.tweet;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
-import android.arch.lifecycle.ViewModel;
+
+import com.crashlytics.android.Crashlytics;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import java.util.List;
 
@@ -10,11 +14,11 @@ import br.com.badiale.tweetmood.R;
 import br.com.badiale.tweetmood.naturallanguage.NaturalLanguageService;
 import br.com.badiale.tweetmood.twitter.TwitterSearchResultStatus;
 import br.com.badiale.tweetmood.twitter.TwitterService;
-import io.reactivex.android.schedulers.AndroidSchedulers;
 
-class TweetListViewModel extends ViewModel {
+class TweetListViewModel extends AndroidViewModel {
     private final TwitterService twitterService;
     private final NaturalLanguageService naturalLanguageService;
+    private final FirebaseAnalytics firebaseAnalytics;
 
     private String userId = "";
 
@@ -22,9 +26,11 @@ class TweetListViewModel extends ViewModel {
     private MutableLiveData<Boolean> loading = new MutableLiveData<>();
     private MutableLiveData<Integer> error = new MutableLiveData<>();
 
-    TweetListViewModel() {
+    TweetListViewModel(Application context) {
+        super(context);
         twitterService = TwitterService.getInstance();
         naturalLanguageService = NaturalLanguageService.getInstance();
+        firebaseAnalytics = FirebaseAnalytics.getInstance(context);
     }
 
     LiveData<List<TwitterSearchResultStatus>> getStatuses() {
@@ -40,10 +46,10 @@ class TweetListViewModel extends ViewModel {
     }
 
     void searchUser(String userId) {
+        firebaseAnalytics.logEvent("search_user", null);
         this.userId = userId;
         loading.setValue(true);
         twitterService.getUserTimeline(userId)
-                .observeOn(AndroidSchedulers.mainThread())
                 .doFinally(() -> loading.setValue(false))
                 .subscribe(twitterSearchResult -> {
                     statuses.setValue(twitterSearchResult.getStatuses());
@@ -51,6 +57,7 @@ class TweetListViewModel extends ViewModel {
     }
 
     void analyse(final TwitterSearchResultStatus tweet) {
+        firebaseAnalytics.logEvent("analyse_tweet", null);
         loading.setValue(true);
         naturalLanguageService.analyse(tweet.getText())
                 .doFinally(() -> loading.setValue(false))
@@ -61,10 +68,12 @@ class TweetListViewModel extends ViewModel {
     }
 
     void refresh() {
+        firebaseAnalytics.logEvent("analyse_tweet", null);
         searchUser(userId);
     }
 
     private void handleError(final Throwable throwable, int resourceId) {
+        Crashlytics.logException(throwable);
         error.setValue(resourceId);
     }
 
